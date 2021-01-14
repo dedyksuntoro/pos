7654fb78ce93627736d78fd32aa30d02d926a4c8
dedyksuntoro <39944254+dedyksuntoro@users.noreply.github.com>
2021-01-14 14:04:48 +0700

    Modifikasi

--- resources/views/layouts/module/sidebar.blade.php
+++ resources/views/layouts/module/sidebar.blade.php
@@ -1,9 +1,9 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="POS" class="brand-image img-circle elevation-3"
+        <img src="{{ asset('dist/img/art-media-bg-trans.png') }}" alt="POS" class="brand-image img-circle elevation-3"
             style="opacity: .8">
-        <span class="brand-text font-weight-light">POS</span>
+        <span class="brand-text font-weight-light">Point Of Sale</span>
     </a>
 
     <!-- Sidebar -->
@@ -14,18 +14,18 @@
                 <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Daengweb.id</a>
+                <a href="#" class="d-block">Artmedia</a>
             </div>
         </div>
 
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <li class="nav-item has-treeview menu-open">
-                    <a href="#" class="nav-link active">
+
+                <li class="nav-item">
+                    <a href="{{ route('home') }}" class="nav-link">
                         <i class="nav-icon fa fa-dashboard"></i>
                         <p>
                             Dashboard
-                            <i class="right fa fa-angle-left"></i>
                         </p>
                     </a>
                 </li>
